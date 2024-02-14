10a324f367cb39a7a135f8b7c68f8477c7ecd132
Valentino <montecinosvalentino2005@hotmail.com>
2024-02-14 15:27:02 -0300

    created genre and game models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      validate: { min: 0, max: 5 },
     },
   });
 };
